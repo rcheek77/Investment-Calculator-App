99a24604ffffa1b7e0db3bdeda5d108c66c02c8b
Richard Cheek <richard_cheekuk@yahoo.co.uk>
2026-07-07 20:41:25 +0100

    Add files via upload

---
+++ header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import investmentlogo from '../images/investmentlogo.png'
+import '../index.css'
+
+export const Header = ({title ="Meet your Financial InvestMate", subtitle="Welcome to the Investment Calculator App"}) => { // default set to prevent the header from being completely blank if you forget to pass a prop
+  return (
+    <header className='header-container'>
+        <img src={investmentlogo} id='headerImg' className='header-logo' alt="logo" />
+        <div className='header-text'>
+          <h1>{title}</h1>
+          <p id='subtitle'>{subtitle}</p>
+        </div>
+
+    </header>
+  )
+}
