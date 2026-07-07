ab68704ed2c997b0e0f937d7857053724eb4d904
Richard Cheek <richard_cheekuk@yahoo.co.uk>
2026-07-07 20:36:31 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Header } from './components/header'
+import './App.css'
+
+function App() {
+  
+  return (
+  <>
+    <Header title="Meet your Financial InvestMate" subtitle="Welcome to the Investment Calculator App"/>
+    <div id='main-title'>
+      <h1>RSC INVESTMENT CALCULATOR ©</h1>
+    </div>
+  </>
+  )
+}
+
+export default App
