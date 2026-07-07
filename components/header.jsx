2d4007288491492e927761da21a6f84b59222d1c
Richard Cheek <richard_cheekuk@yahoo.co.uk>
2026-07-07 20:41:59 +0100

    Add files via upload

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import investmentlogo from '../images/investmentlogo.png'
 import '../index.css'
 
-export const Header = ({title ="Meet your Fincnail InvestMate", subtitle="Welcome to the Investment Calculator App"}) => { // default set to prevent the header from being completely blank if you forget to pass a prop
+export const Header = ({title ="Meet your Financial InvestMate", subtitle="Welcome to the Investment Calculator App"}) => { // default set to prevent the header from being completely blank if you forget to pass a prop
   return (
     <header className='header-container'>
         <img src={investmentlogo} id='headerImg' className='header-logo' alt="logo" />
